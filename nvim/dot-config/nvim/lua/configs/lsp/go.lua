67a19e1a89170d7ccad8c5eca42ce01ebdb24336
Jidechi ukor <10085056+ukor@users.noreply.github.com>
2025-05-16 00:12:01 +0100

    added LSP configuration for golang

---
+++ nvim/dot-config/nvim/lua/configs/lsp/go.lua
@@ -0,0 +1,96 @@
+---@brief
+---
+--- https://github.com/golang/tools/tree/master/gopls
+---
+--- Google's lsp server for golang.
+
+local mod_cache = nil
+
+---@param fname string
+---@return string?
+local function get_root(fname)
+	if mod_cache and fname:sub(1, #mod_cache) == mod_cache then
+		local clients = vim.lsp.get_clients({ name = "gopls" })
+		if #clients > 0 then
+			return clients[#clients].config.root_dir
+		end
+	end
+	return vim.fs.root(fname, "go.work") or vim.fs.root(fname, "go.mod") or vim.fs.root(fname, ".git")
+end
+
+return {
+
+	init_options = {
+		hostInfo = "neovim",
+	},
+	cmd = { "gopls" },
+	single_file_support = false,
+	filetypes = {
+		"go",
+		"gomod",
+		"gowork",
+		"gotmpl",
+	},
+	root_markers = { ".git" },
+	root_dir = function(bufnr, on_dir)
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		-- see: https://github.com/neovim/nvim-lspconfig/issues/804
+		if mod_cache then
+			on_dir(get_root(fname))
+			return
+		end
+		local cmd = { "go", "env", "GOMODCACHE" }
+		vim.system(cmd, { text = true }, function(output)
+			if output.code == 0 then
+				if output.stdout then
+					mod_cache = vim.trim(output.stdout)
+				end
+				on_dir(get_root(fname))
+			else
+				vim.notify(("[gopls] cmd failed with code %d: %s\n%s"):format(output.code, cmd, output.stderr))
+			end
+		end)
+	end,
+	on_attach = function(client, bufnr)
+		--
+
+		vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+		-- See https://github.com/golang/tools/blob/master/gopls/doc/vim.md
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.go",
+			callback = function()
+				local params = vim.lsp.util.make_range_params(0, client.offset_encoding or "utf-16")
+				params.context = { only = { "source.organizeImports" } }
+				-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+				-- machine and codebase, you may want longer. Add an additional
+				-- argument after params if you find that you have to write the file
+				-- twice for changes to be saved.
+				-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+				local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+				for cid, res in pairs(result or {}) do
+					for _, r in pairs(res.result or {}) do
+						if r.edit then
+							local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+							vim.lsp.util.apply_workspace_edit(r.edit, enc)
+						end
+					end
+				end
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+
+		require("core.lsp_attach")
+	end,
+	-- See https://github.com/golang/tools/blob/master/gopls/doc/settings.md
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+			gofumpt = true,
+			usePlaceholders = false,
+		},
+	},
+}
--- nvim/dot-config/nvim/lua/plugins/conform.lua
+++ nvim/dot-config/nvim/lua/plugins/conform.lua
@@ -15,10 +15,15 @@ return { -- Autoformat
 	opts = {
 		notify_on_error = false,
 		format_on_save = function(bufnr)
+			-- Disable autoformat on certain filetypes
+			local ignore_filetypes = { "sql", "java", "go" }
+			if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+				return
+			end
 			-- Disable "format_on_save lsp_fallback" for languages that don't
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true }
+			local disable_filetypes = { c = true, cpp = true, go = true }
 			if disable_filetypes[vim.bo[bufnr].filetype] then
 				return nil
 			else
@@ -30,6 +35,12 @@ return { -- Autoformat
 		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
+			-- Does not seem to work well with gopls
+			-- gopls handles all formating -
+			-- See https://github.com/stevearc/conform.nvim/issues/387
+			-- go = { "gofumpt", "goimports", "goimports-reviser" },
+			--
+			--
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
